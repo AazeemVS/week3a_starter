e5427e8b1e8d7e6d8a19dcb9bb235e4166cb4123
AazeemVS <av7304@rit.edu>
2026-09-08 15:07:21 -0400

    in class demo (Finished outside of class)
    Ran into some problems during class so I  looked at the week 3 notes and competed the demo outside of class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-// add a change
 void main() {
   runApp(const MainApp());
 }
@@ -10,8 +9,50 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return const MaterialApp(home: Week3ALayout());
+  }
+}
+
+class Week3ALayout extends StatelessWidget {
+  const Week3ALayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: const Text('Week 3A App'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 150),
+            Row(
+              children: [
+                const SizedBox(width: 15),
+                Container(
+                  color: Colors.lightGreen,
+                  width: 300,
+                  height: 150,
+                  child: const Text('Hello Week 1'),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(width: 60),
+                Container(
+                  color: Colors.yellow,
+                  width: 220,
+                  height: 155,
+                  child: const Text('Hello Week 2'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
