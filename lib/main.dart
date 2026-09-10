b3e7228266ea92a3736636e3ad2de4fe77b11738
AazeemVS <av7304@rit.edu>
2026-09-10 12:15:33 -0400

    end of class

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,25 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final myScheme = ColorScheme.fromSeed(seedColor: Colors.orange);
+
+    return MaterialApp(
+      theme: ThemeData(
+          colorScheme: myScheme,
+          textTheme: TextTheme(
+              displayLarge: TextStyle(
+                  //fontSize: ,
+                  fontWeight: FontWeight.bold),
+              bodyMedium: TextStyle(
+                  //fontSize: ,
+                  fontWeight: FontWeight.bold))),
+      //elevatedButtonTheme: ElevatedButtonThemeData(
+      //style: ElevatedButton.StyleFrom(color: Colors.red)),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.teal, brightness: Brightness.dark),
+        scaffoldBackgroundColor: Colors.blueGrey[300],
+      ),
       home: Week3App(),
     );
   }
@@ -27,11 +45,15 @@ class Week3App extends StatefulWidget {
 }
 
 class _Week3AppState extends State<Week3App> {
+  void _doSomething() {
+    debugPrint("Method Called");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.yellow,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Week 3A App"),
       ),
       body: SingleChildScrollView(
@@ -43,7 +65,28 @@ class _Week3AppState extends State<Week3App> {
               color: Colors.green,
               child: Text("Hello Week 1"),
             ),
-            SizedBox(height: 30),
+            //SizedBox(height: 30),
+            ElevatedButton(
+                onPressed: () {},
+                child: Text("Elevated Button"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  textStyle:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                )),
+            OutlinedButton(
+                onPressed: () => _doSomething(), child: Text("Outline Button")),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text("Elevated Button with Icon"),
+            ),
+            TextButton(
+                onPressed: () {
+                  debugPrint("Text Button pressed");
+                },
+                child: Text("Text Button")),
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(children: [
